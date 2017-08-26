9ac11cf029222b1230014e38a45345c5e854791c
StefanMarchand <stefanmarchand@stefans-macbook-pro.local>
2017-08-26 11:02:49 -0500

    Initial Commit

---
+++ Solo Mission/GameScene.swift
@@ -0,0 +1,49 @@
+//
+//  GameScene.swift
+//  Solo Mission
+//
+//  Created by Stefan Marchand on 8/26/17.
+//  Copyright © 2017 Stefan Marchand. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    let player = SKSpriteNode(imageNamed: "Spaceship")
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+        
+        player.setScale(1)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
+        player.zPosition = 2
+        self.addChild(player)
+        
+    }
+    
+    func fireBullet(){
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
+    }
+        
+        
+    }
+    
+        
